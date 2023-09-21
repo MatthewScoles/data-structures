9f150f8e7c1aa7e90efcc731e1ceefacf3e13f45
MatthewScoles <msscoles@stu.naperville203.org>
2023-09-21 13:30:51 -0500

    Sieve

--- Chapter 15 Activities/Sieve/src/Sieve.java
+++ Chapter 15 Activities/Sieve/src/Sieve.java
@@ -1,5 +1,7 @@
 import java.util.Scanner;
-
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
 
 /**
  * A program that implements the sieve of Eratosthenes.
@@ -13,9 +15,27 @@ public class Sieve
         int n = in.nextInt();
 
         // Your work goes here
-        . . .
+        
 
+        Set<Integer> nums = new HashSet<>();
+        int i = 2;
+        while(i<=n){
+            nums.add(i);
+            i++;
+        }
+        i = 2;
+        
+            Iterator iterator = nums.iterator();
+            while(iterator.hasNext()){
+                int x = (int) iterator.next();
+                if (x % 2 == 0 || x % 3 == 0 ) {
+                    iterator.remove();
+                }
+            }
 
+            i++;
+
+        System.out.println(nums);
 
 
 
--- Chapter 15 Class Notes/src/WordAnalysis.java
+++ Chapter 15 Class Notes/src/WordAnalysis.java
@@ -10,9 +10,28 @@ import java.io.FileNotFoundException;
 */
 public class WordAnalysis
 {
-    public static void main(String[] args)
+    public static void main(String[] arg)
         throws FileNotFoundException
     {
+        Set<String> dictionaryWords = readWords("Chapter 15 Class Notes/src/words");
+        Set<String> novelWords = readWords("Chapter 15 Class Notes/src/throughTheLookingGlass.txt");
+    
+        int count = 0; 
+
+        for (String word: novelWords){
+            if (!dictionaryWords.contains(word)){
+                System.out.println(word);
+            } count++;
+            
+        }
+        System.out.println("There are " + novelWords.size()+" unique words in the novel");
+        Iterator<String> iterator = novelWords.iterator();
+        while (iterator.hasNext()){
+            if (iterator.next().length() <= 3){
+                iterator.remove();
+            }
+        }
+        System.out.println("There are " + novelWords.size()+" unique words in the novel");
     }
 
     /**
@@ -25,6 +44,17 @@ public class WordAnalysis
     public static Set<String> readWords(String filename)
         throws FileNotFoundException
     {
-        return null;
+        Set<String> words = new HashSet<>();
+
+        Scanner in = new Scanner(new File(filename), "UTF-8");
+
+        in.useDelimiter("[^a-zA-Z]+");
+
+        while( in.hasNext())
+        {
+            words.add(in.next().toLowerCase());
+        }
+
+        return words;
     }
 }
